7c1feba2319184c25ea577341822ad07a6b7f811
Benjamin Looi <benjaminlooi97@gmail.com>
2026-05-03 17:13:57 +0700

    fix: use TanStackRouterVite instead of TanStack Start

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
@@ -11,7 +11,7 @@ const config = defineConfig({
 		devtools(),
 		tailwindcss(),
 		process.env.VITEST ? null : cloudflare({ viteEnvironment: { name: "ssr" } }),
-		tanstackStart(),
+		TanStackRouterVite(),
 		viteReact(),
 	],
 });
